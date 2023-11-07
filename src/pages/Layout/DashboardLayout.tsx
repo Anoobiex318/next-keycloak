8ff1b3dd95a4fc0d06c095ebee1a20f4215a4155
Anoobiex318 <elitepro318@gmail.com>
2023-11-07 23:11:45 +0800

    updated spinner and skeleton animation logic

--- src/pages/Layout/DashboardLayout.tsx
+++ src/pages/Layout/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   AppBar,
   Toolbar,
@@ -19,8 +19,11 @@ import {
 import MenuIcon from '@mui/icons-material/Menu'; // Hamburger icon
 import HomeIcon from '@mui/icons-material/Home';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { signOut, useSession } from 'next-auth/react';
 import LogoutIcon from '@mui/icons-material/Logout';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useRouter } from 'next/router';
 
 
 interface DashboardLayoutProps {
@@ -30,23 +33,38 @@ interface DashboardLayoutProps {
 const drawerWidth = 240;
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const [mobileOpen, setMobileOpen] = useState(false);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSigningOut, setIsSigningOut] = useState(false);
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
+  const router = useRouter();
   const handleSignOut = async () => {
     const keycloakIssuer = process.env.NEXT_PUBLIC_KEYCLOAK_ISSUER;
     const postLogoutRedirectUri = encodeURIComponent(`${window.location.origin}/`);
+    setIsSigningOut(true); // Start the loading spinner
 
     await signOut({ redirect: false });
     const keycloakLogoutUrl = `${keycloakIssuer}/protocol/openid-connect/logout?redirect_uri=${postLogoutRedirectUri}`;
     window.location.href = keycloakLogoutUrl;
   };
+   
+  useEffect(() => {
+    // A short delay of 500 milliseconds
+    const timer = setTimeout(() => setIsLoading(false), 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  useEffect(() => {
+    // If the session status is "unauthenticated", redirect to the login page
+    if (status === 'unauthenticated') {
+      router.push('/');
+    }
+  }, [status, router]);
 
   const drawerContent = (
     <Box onClick={handleDrawerToggle}  sx={{
@@ -75,6 +93,42 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
     <CssBaseline />
+    {isLoading && (
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            zIndex: 1400, // Ensure this is higher than AppBar's zIndex
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <CircularProgress size="5rem" />
+          </Box>
+        )}
+         {isSigningOut && (
+        <Box
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            zIndex: 2000, // Make sure this is higher than everything else
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <CircularProgress size="5rem" />
+        </Box>
+      )}
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
     <Toolbar>
           {isMobile && (
@@ -92,9 +146,11 @@ const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
             Dashboard
           </Typography>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
+    
             <Typography variant="subtitle1" noWrap component="div" sx={{ margin: '0 10px' }}>
-             Welcome: <strong>{session?.user?.keycloakUsername ?? 'Unknown User'}</strong> 
+             Welcome: <strong>{session?.user?.keycloakUsername ?? 'Unknown User'} </strong> 
             </Typography>
+            <AccountCircleIcon sx={{ fontSize: '2rem' }} />
             <Button color="inherit" onClick={handleSignOut}>
             <Tooltip title="Sign Out">
              <LogoutIcon/>
